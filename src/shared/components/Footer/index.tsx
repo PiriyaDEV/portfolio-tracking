5a7e41c5d7d22c58510a249fabebff200d7aff2d
Piriya Chaigul <piriya.chaigul@ibm.com>
2025-11-19 17:36:37 +0700

    fix: fix open

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -29,7 +29,7 @@ export default function FooterPortfolio({
   formattedDate,
   getProfitColor,
 }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   if (!assets) return null;
 
